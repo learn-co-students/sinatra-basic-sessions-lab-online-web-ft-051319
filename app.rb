6d153b6c01d5513b8552897e38e30e2cca6dccaa
Brian <briandsalemi@gmail.com>
2019-06-28 10:59:21 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+     configure do
+        enable :sessions 
+        set :session_secret, "pickle"
+     end 
+
+    get '/' do 
+        erb :index
+    end 
+
+    post '/checkout' do
+        @session = session 
+        @session[:item] = params[:item]
+        binding.pry
+
+    end 
+
 end
