84064b8eea4670448f17e1e713f99a025a239690
Brian <briandsalemi@gmail.com>
2019-06-28 11:06:34 -0400

    Done.

--- app.rb
+++ app.rb
@@ -12,10 +12,9 @@ class App < Sinatra::Base
     end 
 
     post '/checkout' do
+        session[:item] = params[:item]
         @session = session 
-        @session[:item] = params[:item]
-        binding.pry
-
+        erb :checkout
     end 
 
 end
