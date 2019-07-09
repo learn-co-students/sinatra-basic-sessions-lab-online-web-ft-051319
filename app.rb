07427039158a5071bd92d38926f2285468d278d6
Kane Koo <kanekoo@gmail.com>
2019-07-08 21:10:50 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,25 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "sucret"
+  end
+
+  get '/' do
+
+    erb :index
+  end
+
+  post '/checkout' do
+    @sesshash={}
+    # @temphash[params[:item]] = params[:price]
+    #     binding.pry
+    # session << @temphash
+    session[:item] = params[:item]
+    @sesshash = session
+    erb :display_items
+  end
 end
