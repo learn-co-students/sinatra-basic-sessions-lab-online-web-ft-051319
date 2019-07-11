baf712ab00ee41601770a652f330eb8b33bce31e
Alethea Cary <aletheacary@gmail.com>
2019-07-11 04:07:15 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,13 +4,15 @@ class App < Sinatra::Base
   
   configure do
     enable :sessions
-    set :session_secret, "secret"
+    set :session_secret, "item"
   end
   
   get ('/'){erb :index}
   
   post '/checkout' do 
+    @session = session
     @item = item
+    @session[:item] = @item
     
     
     
